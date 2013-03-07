3c7566e6b7fda564f6ff93ff66120bafaa229c75
Oliver Phillip Roer <phazyck@gmail.com>
2013-03-07 21:42:20 +0100

    Project created.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RentItProject.RentItReference;
+
+namespace RentItProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new RentItServiceClient())
+            {
+                // Insert code here! =D
+            }
+        }
+    }
+}
